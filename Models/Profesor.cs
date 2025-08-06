d67056dcc8fccc553fd59b6ade666f1b89d13ea6
tamaki094 <edri.alan.94@gmail.com>
2025-08-06 14:55:17 -0600

    se agrega creacion de cursos

--- Models/Profesor.cs
+++ Models/Profesor.cs
@@ -6,8 +6,7 @@ namespace API_Cursos.Models
     public class Profesor
     {
         [Key]
-        [JsonIgnore]
-        public int IdProfesor { get; set; }
+        public int? IdProfesor { get; set; }
         public string Nombre { get; set; } = string.Empty;
         public string ApellidoPaterno { get; set; } = string.Empty;
         public string ApellidoMaterno { get; set; } = string.Empty;
